3fdb9382f7e2615d7068dcc7dd8c8404d1b00994
Brayan <Brayan@192.168.1.2>
2021-07-02 21:03:53 -0500

    Examen1 ProgramacionTI

--- src/examen1Progra1TI/Examen1.java
+++ src/examen1Progra1TI/Examen1.java
@@ -13,10 +13,11 @@ import java.util.Scanner;
  */
 public class Examen1 {
     public static void main(String[] args){
-        examen1Progra();
+        examen1();
+        
     }
     
-    public static void examen1Progra(){
+    public static void examen1(){
         Scanner escaner = new Scanner(System.in);
         
         System.out.println("Digite el nombre de la empresa: ");
@@ -25,6 +26,31 @@ public class Examen1 {
         System.out.println("Digite la provincia de la empresa: ");
         System.out.println("1-San Jose 2-Alajuela 3-Cartago 4-Heredia 5-Guanacaste 6-Puntarenas 7-Limon");
         int numProvincia = escaner.nextInt();
+        String nombreProvincia;
+        int poblacion;
+            if(numProvincia == 1){
+                nombreProvincia = "San Jose";
+                poblacion = 1404242;
+            }
+            else if(numProvincia == 2){
+                nombreProvincia = "Alajuela";
+            }
+            else if(numProvincia == 3){
+                nombreProvincia = "Cartago";
+            }
+            else if(numProvincia == 4){
+                nombreProvincia = "Heredia";
+            }
+            else if(numProvincia == 5){
+                nombreProvincia = "Guanacaste";
+            }
+            else if(numProvincia == 6){
+                nombreProvincia = "Puntarenas";
+            }
+            else{
+                nombreProvincia = "Limon";
+            }
+            
         
         System.out.println("Digite el numero total de trabajadores: ");
         int numTotalTrabajadores = escaner.nextInt();
@@ -35,39 +61,47 @@ public class Examen1 {
         System.out.println("Digite el monto de ingreso por mes: ");
         int montoDeIngresoMes = escaner.nextInt();
         
-        System.out.println(" La empresa cuenta con creditos?");
+        System.out.println("La empresa cuenta con creditos?");
         System.out.println("1-Si o 2-No");
         
         int tieneCreditos = escaner.nextInt();
-        int credito;
-        int nuevoCredito;
+        int credito = 0;
+        int nuevoCredito = 0;
         
         while(tieneCreditos == 1){
             System.out.println("Digite el valor del credito");
             credito = escaner.nextInt();
-            
+            tieneCreditos++;
+        }
+        System.out.println("Tiene mas creditos?");
+        System.out.println("1-Si o 2-No");
+        int masCreditos = escaner.nextInt();
+        while(masCreditos == 1){
+            System.out.println("Digite su credito:");
+            nuevoCredito = escaner.nextInt()+ nuevoCredito;
             System.out.println("Tiene mas creditos?");
             System.out.println("1-Si o 2-No");
-            
-            int masCreditos = escaner.nextInt();
-            if(masCreditos > 1){
-                tieneCreditos = 2;
-            }
-            while(masCreditos == 1){
-                System.out.println("Digite su credito2: ");
-                nuevoCredito = escaner.nextInt();
-                break;
-            }
-            
+            masCreditos = escaner.nextInt();
         }
         
-        int creditoTotal = credito + nuevoCredito;
         
-        System.out.println("Credito total:" + creditoTotal);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Nombre de la empresa: " + nombreEmpresa);
+        System.out.println("Provincia: " + nombreProvincia);
+        System.out.println("Total de Trabajadores: " + numTotalTrabajadores);
+        if(numTrabajadoresCovid >= 20){
+            System.out.println("20 o mas trabajadores contagiados de COVID-19");
+        }
+        else{
+            System.out.println("Numero de contagiados Covid-19" + numTrabajadoresCovid);
+        }
+        System.out.println("Monto de ingreso al mes" + montoDeIngresoMes);
         
-        
-        
-        
+        System.out.print("Credito total:");
+        System.out.println(credito + nuevoCredito);
+        //todo perfecto hasta aqui
         
         
     }
